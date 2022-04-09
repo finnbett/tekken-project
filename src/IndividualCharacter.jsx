b71c78fc1c3b3b4d4b3f1cdb0fdf24d76728bce1
Finn <fluxr3@gmail.com>
2022-04-10 11:22:48 +1200

    title case characters

--- src/IndividualCharacter.jsx
+++ src/IndividualCharacter.jsx
@@ -15,6 +15,9 @@ export function IndividualCharacter() {
     const [characterData, setCharacterData] = useState([{"command":"d/b or d+3 or FC+3","hitRange":"l (TC)","damage":"7","startup":"15s cs6or1~","hit":"-2","block":"-11","counterHit":"-2","notes":""}])
     let location = useLocation()
     console.log(location.state)
+    let splitTitle = (location.state).split('')
+    let titleFirstChar = splitTitle[0].toUpperCase()
+    splitTitle[0] = titleFirstChar
     useEffect(() => {
         request.get(`http://tkn-api.herokuapp.com/character/${location.state}`)
         .then(res => res.body)
@@ -27,7 +30,7 @@ export function IndividualCharacter() {
     }, [])
     return(
         <>
-        <h1>{location.state}</h1>
+        <h1>{splitTitle}</h1>
         {characterData.map(data =>{
             return <li key={Math.random()}>{data.command}</li>
         })}
